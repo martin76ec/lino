a6ec1261c79bb8f6334bb5bd0320f79aced92037
martin <martin@localhost.localdomain>
2021-02-26 04:44:16 -0500

    feat: added serverjs with initial configuration of express

---
+++ app/server/server.js
@@ -0,0 +1,19 @@
+const http = require('http')
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.server = http.createServer(app)
+app.use(cors())
+app.use(bodyParser.json({ extended: false }))
+
+app.get('/', (request, response) => {
+	response.send('<h1>Hello World, this is from MEME</h1>')
+})
+
+app.server.listen(process.env.PORT || 3000)
+console.log(`Server started at port: ${ app.server.address().port }`)
+
+module.exports = app
